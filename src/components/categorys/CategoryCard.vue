08d6e5b1c40aa1918800439d837ea09c0d214028
yeonso08 <hjng0825@gmail.com>
2023-09-26 14:21:32 +0900

    feat : categoryCard 생성 및 데이터 바인딩 연결

---
+++ src/components/categorys/CategoryCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <b-card :title="item.title" :img-src="item.image" img-alt="Image" img-top tag="article" style="max-width: 20rem;"
+            class="mb-2">
+            <b-card-sub-title class="mb-2">{{ item.category }}</b-card-sub-title>
+            <b-card-text>
+                <span>****</span>
+                <span>{{ item.rating.count }}ratings</span>
+            </b-card-text>
+            <b-card-text class="d-flex justify-content-between">
+                <span>${{ item.price }}</span>
+                <span>HEART</span>
+            </b-card-text>
+        </b-card>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        item: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
--- src/views/category/CategoryView.vue
+++ src/views/category/CategoryView.vue
@@ -1,13 +1,35 @@
 <template>
     <div>
-        test
+        <CategoryCard v-for="product in CategoryProducts" :key="product.id" :item="product" />
     </div>
 </template>
 
 <script>
+import CategoryCard from '@/components/categorys/CategoryCard.vue'
+import { Api } from '@/api/Api';
 export default {
+    components: {
+        CategoryCard,
+    },
+    data() {
+        return {
+            CategoryProducts: []
+        }
+    },
+    methods: {
+        fetchCategoryProducts(categoryId) {
+            Api.getCategoryProducts(categoryId).then(res => {
+                console.log("Received categories:", res.data);
+                this.CategoryProducts = res.data;
+            }).catch(error => {
+                console.error("API 호출 중 에러 발생:", error);
+            });
+        }
+    },
+    mounted() {
+        const categoryId = this.$route.params.id;
+        this.fetchCategoryProducts(categoryId);
+    }
 
 }
 </script>
-
-<style></style>
